7a79770a9046b61f89ac4257942b26839aa74dfc
justKhanaa@gmail.com <justKhanaa@gmail.com>
2020-06-15 16:43:42 +0530

    fixed some difect and change the site

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@ particlesJS("particles-js",
     {
         "particles": {
             "number": {
-                "value": 40,
+                "value": 50,
                 "density": { "enable": true, "value_area": 750 }
             },
             "color": { "value": "#55ff06" },
             "shape": {
-                "type": "circle",
+                "type": "polygon",
                 "stroke": { "width": 0, "color": "#eeeeee" },
-                "polygon": { "nb_sides": 5 },
+                "polygon": { "nb_sides": 6 },
                 "image": { "src": "img/github.svg", "width": 100, "height": 1000 }
             },
             "opacity": {
@@ -17,16 +17,16 @@ particlesJS("particles-js",
                 "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false }
             },
             "size": { "value": 4, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } },
-            "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 },
+            "line_linked": { "enable": true, "distance": 100, "color": "#ffffff", "opacity": 0.4, "width": 1 },
             "move": {
                 "enable": true, "speed": 4, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false,
-                "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 }
+                "attract": { "enable": false, "rotateX": 1000, "rotateY": 1200 }
             }
         },
         "interactivity": {
             "detect_on": "window",
             "events": {
-                "onhover": { "enable": true, "mode": "grab" },
+                "onhover": { "enable": true, "mode": "bubble" },
                 "onclick": { "enable": true, "mode": "remove" }, "resize": true
             },
             "modes": {
@@ -34,7 +34,7 @@ particlesJS("particles-js",
                     "distance": 231.44200550588337,
                     "line_linked": { "opacity": 1 }
                 },
-                "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 },
+                "bubble": { "distance": 400, "size": 10, "duration": 2, "opacity": 8, "speed": 3 },
                 "repulse": { "distance": 200, "duration": 0.4 },
                 "push": { "particles_nb": 4 },
                 "remove": { "particles_nb": 2 }
@@ -134,6 +134,11 @@ function contactRoute() {
   let elmnt = document.getElementById("myContacts");
   elmnt.scrollIntoView();
 }  
+function feedbackRoute(){
+    let elmnt = document.getElementById("myfeedback");
+    elmnt.scrollIntoView();
+  } 
+
 function resumeRoute() {
     window.open('https://drive.google.com/file/d/1DDyK6gkRZreXvlvh33CkDqlR_k042Kzv/view?usp=sharing', '_blank');
     
